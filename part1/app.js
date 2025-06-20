b93dc03abd96d3589c02e6b4c81d8817ea540333
Blake6666 <a1902292@adelaide.edu.au>
2025-06-20 01:20:04 +0000

    [automatic commit] 2025-06-20 01:20:04 UTC 104.28.211.79

--- part1/app.js
+++ part1/app.js
@@ -115,7 +115,23 @@ app.get('/api/dogs', async (req, res) => {
 });
 
 app.get('/api/walkrequests/open', (req, res) => {
-
+  try {
+    const [requests] = await db.execute(`
+    SELECT
+    d.name AS dog_name,
+    d.size,
+    u.username AS owner_username
+    FROM
+    Dogs d
+    JOIN
+    Users u ON d.owner_id = u.user_id
+    ORDER BY
+    u.username, d.name;
+      `);
+    res.json(requests);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch' });
+  }
 });
 
 app.use('/', indexRouter);
