37cb4b845b67f1a44a22d8692578fce0fc771f73
Blake6666 <a1902292@adelaide.edu.au>
2025-06-20 01:00:19 +0000

    [automatic commit] 2025-06-20 01:00:19 UTC 104.28.211.79

--- part1/app.js
+++ part1/app.js
@@ -42,6 +42,20 @@ let db;
       SET @alice_id = (SELECT user_id FROM Users WHERE username = 'alice123');
 SET @carol_id = (SELECT user_id FROM Users WHERE username = 'carol123');
 SET @emma_id  = (SELECT user_id FROM Users WHERE username = 'emmaowner');
+      INSERT INTO Dogs (owner_id, name, size) VALUES
+(@alice_id, 'Max', 'medium'),
+(@carol_id, 'Bella', 'small'),
+(@alice_id, 'Rocky', 'large'),
+(@emma_id, 'Lucy', 'small'),
+(@carol_id, 'Charlie', 'medium');
+
+SET @max_id = (SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = @alice_id);
+SET @bella_id = (SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = @carol_id);
+SET @rocky_id = (SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = @alice_id);
+SET @lucy_id = (SELECT dog_id FROM Dogs WHERE name = 'Lucy' AND owner_id = @emma_id);
+SET @charlie_id = (SELECT dog_id FROM Dogs WHERE name = 'Charlie' AND owner_id = @carol_id);
+
+
     `);
 
   } catch (err) {
