6a7b619be40db0404e7f5e6a1eee33f03916c5f3
Blake6666 <a1902292@adelaide.edu.au>
2025-06-20 02:45:46 +0000

    [automatic commit] 2025-06-20 02:45:46 UTC 104.28.211.79

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,8 @@ const app = express();
 // Middleware
 app.use(logger('dev'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
