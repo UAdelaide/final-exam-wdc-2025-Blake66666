a1242747b000cb13ad2fa68e4db15a4331810f49
Blake6666 <a1902292@adelaide.edu.au>
2025-06-20 02:50:53 +0000

    [automatic commit] 2025-06-20 02:50:53 UTC 104.28.211.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,9 +38,11 @@ router.get('/me', (req, res) => {
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  console.log(username);
-  console.log(password);
+
   try {
+    console.log(username);
+    console.log(password);
+
     console.log("haah");
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
